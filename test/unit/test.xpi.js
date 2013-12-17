0f00cb4801a1ec35648987c1a89f86bf1830b103
Jordan Santell <jsantell@gmail.com>
2013-12-16 20:30:59 -0800

    Refactor tests with more utils

--- test/unit/test.xpi.js
+++ test/unit/test.xpi.js
@@ -1,7 +1,7 @@
 var fs = require("fs");
 var path = require("path");
 var rimraf = require("rimraf");
-var unzip = require("unzip");
+var utils = require("../utils");
 var chai = require("chai");
 var expect = chai.expect;
 var xpi = require("../../lib/xpi");
@@ -11,51 +11,20 @@ var tmpOutputDir = path.join(__dirname, "../", "tmp");
 var prevCwd;
 
 describe("lib/xpi", function () {
-  // Before each test, make the temp directory and save cwd
-  beforeEach(function (done) {
-    prevCwd = process.cwd();
-    fs.mkdir(tmpOutputDir, done);
-  });
-
-  // After each test, revert to previous cwd and nuke the temp directory
-  afterEach(function (done) {
-    process.chdir(prevCwd);
-    rimraf(tmpOutputDir, function () {
-      cleanXPI(done);
-    });
-  });
+  beforeEach(utils.setup);
+  afterEach(utils.tearDown);
 
   it("Zips up cwd's addon", function (done) {
     process.chdir(simpleAddonPath);
     var manifest = require(path.join(simpleAddonPath, "package.json"));
     xpi(manifest).then(function (xpiPath) {
       expect(xpiPath).to.be.equal(path.join(simpleAddonPath, "simple-addon.xpi"));
-      fs.createReadStream(xpiPath)
-        .pipe(unzip.Extract({ path: tmpOutputDir }))
-        .on('close', function () {
-          compareDir(simpleAddonPath, tmpOutputDir, done);
-        });
+      utils.unzipTo(xpiPath, tmpOutputDir, function () {
+        utils.compareDirs(simpleAddonPath, tmpOutputDir, done);
+      });
     }).then(null, done);
   });
 });
-
-function filterXPI (filename) {
-  return !/\.xpi$/.test(filename);
-}
-
-function compareDir (dir1, dir2, done) {
-  var files1 = fs.readdirSync(dir1).filter(filterXPI);
-  var files2 = fs.readdirSync(dir2).filter(filterXPI);
-
-  expect(files1.length).to.be.equal(files2.length);
-
-  files1.forEach(function (file) {
-    var s1 = fs.readFileSync(path.join(dir1, file), "utf-8");
-    var s2 = fs.readFileSync(path.join(dir2, file), "utf-8");
-    expect(s1).to.be.equal(s2);
-  });
-  done();
-}
 
 function cleanXPI (done) {
   fs.unlinkSync(path.join(simpleAddonPath, "simple-addon.xpi"));
--- test/utils/index.js
+++ test/utils/index.js
@@ -2,6 +2,10 @@ var path = require("path");
 var execFile = require("child_process").execFile;
 var fs = require("fs");
 var rimraf = require("rimraf");
+var glob = require("glob");
+var unzip = require("unzip");
+var chai = require("chai");
+var expect = chai.expect;
 var prevCwd;
 
 var tmpOutputDir = exports.tmpOutputDir = path.join(__dirname, "../", "tmp");
@@ -13,10 +17,16 @@ function setup (done) {
 }
 exports.setup = setup;
 
-// After each test, revert to previous cwd and nuke the temp directory
+// After each test, revert to previous cwd, nuke the temp directory
+// and clear out any XPIs in the `./test/addons/` directory
 function tearDown (done) {
   process.chdir(prevCwd);
-  rimraf(tmpOutputDir, done);
+  rimraf(tmpOutputDir, function () {
+    glob(path.join(__dirname, "../addons/**/*.xpi"), function (err, xpis) {
+      xpis.forEach(fs.unlinkSync);
+      done();
+    });
+  });
 }
 exports.tearDown = tearDown;
 
@@ -29,3 +39,30 @@ function exec (args) {
   });
 }
 exports.exec = exec;
+
+function unzipTo (xpiPath, outputDir, callback) {
+  fs.createReadStream(xpiPath)
+    .pipe(unzip.Extract({ path: outputDir }))
+    .on('close', callback);
+}
+exports.unzipTo = unzipTo;
+
+
+function filterXPI (filename) {
+  return !/\.xpi$/.test(filename);
+}
+
+function compareDirs (dir1, dir2, done) {
+  var files1 = fs.readdirSync(dir1).filter(filterXPI);
+  var files2 = fs.readdirSync(dir2).filter(filterXPI);
+
+  expect(files1.length).to.be.equal(files2.length);
+
+  files1.forEach(function (file) {
+    var s1 = fs.readFileSync(path.join(dir1, file), "utf-8");
+    var s2 = fs.readFileSync(path.join(dir2, file), "utf-8");
+    expect(s1).to.be.equal(s2);
+  });
+  done();
+}
+exports.compareDirs = compareDirs;
