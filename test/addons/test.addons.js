d755b01362a8f141d4ecbed261857ed8fc9dd975
Erik Vold <evold@mozilla.com>
2014-10-23 19:26:48 -0700

    jpm test addons cleanup

--- test/addons/test.addons.js
+++ test/addons/test.addons.js
@@ -12,18 +12,15 @@ var assert = chai.assert;
 var exec = utils.exec;
 
 var addonsPath = path.join(__dirname, "..", "addons");
-
 var binary = process.env.JPM_FIREFOX_BINARY || "nightly";
 
 describe("jpm test addons", function () {
   beforeEach(utils.setup);
   afterEach(utils.tearDown);
 
-  fs.readdirSync(addonsPath).filter(function (file) {
-    if (/^invalid-/.test(file)) return false;
-    var stat = fs.statSync(path.join(addonsPath, file))
-    return (stat && stat.isDirectory());
-  }).forEach(function (file) {
+  fs.readdirSync(addonsPath)
+  .filter(fileFilter)
+  .forEach(function (file) {
     it(file, function (done) {
       var addonPath = path.join(addonsPath, file);
       process.chdir(addonPath);
@@ -32,7 +29,7 @@ describe("jpm test addons", function () {
       if (process.env.DISPLAY) {
         options.env.DISPLAY = process.env.DISPLAY;
       }
-      var proc = exec("test -v", options, function (err, stdout, stderr) {
+      var proc = exec("test", options, function (err, stdout, stderr) {
         expect(err).to.not.be.ok;
         expect(stdout).to.contain("All tests passed!");
         done();
@@ -40,3 +37,11 @@ describe("jpm test addons", function () {
     });
   });
 });
+
+function fileFilter(file) {
+  if (/^invalid-/.test(file)) {
+    return false;
+  }
+  var stat = fs.statSync(path.join(addonsPath, file))
+  return (stat && stat.isDirectory());
+}
