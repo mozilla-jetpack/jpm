71ad88563f318f65c860f3f1c72cfe167267c18d
Erik Vold <evold@mozilla.com>
2014-11-07 14:29:18 -0800

    Issue #186 updating the unpacked test add-on

--- test/addons/unpacked-addon/test/test-main.js
+++ test/addons/unpacked-addon/test/test-main.js
@@ -7,18 +7,18 @@ const self = require("sdk/self");
 const url = require("sdk/url");
 const { getAddonByID } = require("sdk/addon/manager");
 
-exports["test self.packed"] = function (assert) {
+exports["test self.packed"] = (assert) => {
   assert.ok(!self.packed, "require('sdk/self').packed is correct");
 }
 
-exports["test url.toFilename"] = function (assert) {
+exports["test url.toFilename"] = (assert) => {
   assert.ok(/.*main\.js$/.test(url.toFilename(module.uri)),
             "url.toFilename() on resource: URIs should work");
 }
 
 exports["test Addon is unpacked"] = function*(assert) {
   let addon = yield getAddonByID(self.id);
-  assert.ok(addon.unpacked, "the addon is unpacked");
+  assert.equal(addon.getResourceURI("").scheme, "file", "the addon is unpacked");
 }
 
-require("sdk/test").run(module);
+require("sdk/test").run(exports);
