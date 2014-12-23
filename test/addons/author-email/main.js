54ab5bcc0d997897a8cba4a50b20050106132fa8
Erik Vold <evold@mozilla.com>
2014-12-23 12:59:35 -0800

    Adding tests that addon.hasResource is working correctly with jpmignored files

--- test/addons/author-email/main.js
+++ test/addons/author-email/main.js
@@ -1,14 +1,18 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
-'use strict';
+"use strict";
 
-const { id } = require('sdk/self');
-const { getAddonByID } = require('sdk/addon/manager');
+const { id } = require("sdk/self");
+const { getAddonByID } = require("sdk/addon/manager");
+const { author } = require("./package.json");
 
-exports.testContributors = function*(assert) {
+const AUTHOR = "test <test@mozilla.com>";
+
+exports.testAuthorEmail = function*(assert) {
   let addon = yield getAddonByID(id);
-  assert.equal(addon.creator.name, 'test <test@mozilla.com>', '< and > characters work');
+  assert.equal(addon.creator.name, AUTHOR, "< and > characters work");
+  assert.equal(author, AUTHOR, "< and > characters work");
 }
 
-require('sdk/test/runner').runTestsFromModule(module);
+require("sdk/test/runner").runTestsFromModule(module);
