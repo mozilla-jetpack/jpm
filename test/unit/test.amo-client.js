059d6b6f4ba4b607abb81e9c5ae9d4ba444d0172
Kumar McMillan <kumar.mcmillan@gmail.com>
2015-10-23 10:49:20 -0500

    Wait for validation and show signed XPI

--- test/unit/test.amo-client.js
+++ test/unit/test.amo-client.js
@@ -9,188 +9,409 @@ var expect = chai.expect;
 var jwt = require('jsonwebtoken');
 var when = require("when");
 
-var utils = require("../utils");
-var AMOClient = require('../../lib/amo-client').Client;
+var amoClient = require('../../lib/amo-client');
 
 
 describe('amoClient.Client', function() {
 
-  beforeEach(function() {
-    utils.setup();
+  function setUp() {
     var self = this;
-
-    this.client = new AMOClient({
+    this.client = new amoClient.Client({
       apiKey: 'fake-api-key',
       apiSecret: 'fake-api-secret',
       apiUrlPrefix: 'http://not-a-real-amo-api.com/api/v3',
+      signedStatusCheckInterval: 0,
       fs: {
         createReadStream: function() {
           return 'fake-read-stream';
         }
       },
       request: new MockRequest(),
+      validateProgress: new MockProgress(),
+    });
+  }
+
+  describe('signing', function() {
+
+    beforeEach(function() {
+      setUp.call(this);
+      var self = this;
+
+      this.sign = function(conf) {
+        conf = _.assign({}, {
+          guid: 'some-guid',
+          version: 'some-version',
+          xpiPath: 'some-xpi-path',
+        }, conf);
+        return self.client.sign(conf);
+      };
+
     });
 
-    this.sign = function(conf) {
-      conf = _.assign({}, {
-        guid: 'some-guid',
-        version: 'some-version',
-        xpiPath: 'some-xpi-path',
-      }, conf);
-      return self.client.sign(conf);
-    };
-  });
+    function signedResponse(overrides) {
+      var res = _.assign({
+        processed: true,
+        valid: true,
+        active: true,
+        files: [{
+          download_url: 'http://amo/some-signed-file-1.2.3.xpi',
+        }],
+        validation_url: 'http://amo/validation-results/',
+      }, overrides);
 
-  afterEach(utils.tearDown);
+      return {
+        responseBody: res,
+      }
+    }
 
-  it('lets you sign an add-on', function(done) {
-    var self = this;
-    var conf = {
-      guid: 'a-guid',
-      version: 'a-version',
-    };
+    it('lets you sign an add-on', function(done) {
+      var self = this;
+      var conf = {
+        guid: 'a-guid',
+        version: 'a-version',
+      };
+      var waitForSignedAddon = new CallableMock();
+      this.client.waitForSignedAddon = waitForSignedAddon.getCallable();
 
-    this.sign(conf).then(function() {
-      var call = self.client._request.calls[0];
-      expect(call.name).to.be.equal('put');
-      expect(call.conf.url).to.include(
-          '/addons/' + conf.guid + '/versions/' + conf.version);
-      expect(call.conf.formData.upload).to.be.equal('fake-read-stream');
-      done();
-    }).catch(done);
-  });
+      this.sign(conf).then(function() {
+        var putCall = self.client._request.calls[0];
+        expect(putCall.name).to.be.equal('put');
 
-  it('makes requests with an auth token', function(done) {
-    var self = this;
+        var partialUrl = '/addons/' + conf.guid + '/versions/' + conf.version;
+        expect(putCall.conf.url).to.include(partialUrl);
+        expect(putCall.conf.formData.upload).to.be.equal('fake-read-stream');
 
-    this.sign().then(function() {
-      var call = self.client._request.calls[0];
-      var headerMatch = call.conf.headers.Authorization.match(/JWT (.*)/);
-      var token = headerMatch[1];
-      var data = jwt.verify(token, self.client.apiSecret);
-      expect(data.iss).to.be.equal(self.client.apiKey);
-      expect(data).to.have.keys(['iss', 'iat', 'exp']);
-      done();
-    }).catch(done);
-  });
+        expect(waitForSignedAddon.wasCalled).to.be.equal(true);
+        expect(waitForSignedAddon.call[0]).to.include(partialUrl);
 
-  it('can make any HTTP request', function(done) {
-    var self = this;
-    var requests = [];
-    ['get', 'put', 'post', 'patch', 'delete'].forEach(function(method) {
-      var urlPath = '/some/path';
-
-      requests.push(self.client[method]({url: urlPath}).then(function() {
-        var call = self.client._request.callMap[method];
-        expect(call.conf.url).to.be.equal(self.client.apiUrlPrefix + urlPath);
-        expect(call.conf.headers).to.have.keys(['Accept', 'Authorization']);
-      }));
-
+        done();
+      }).catch(done);
     });
-    when.all(requests).then(function() { done() }).catch(done);
-  });
 
-  it('rejects the request promise on > 200 responses', function(done) {
-    this.client._request = new MockRequest({httpResponse: {statusCode: 409}});
-    this.client.get({url: '/something'}).then(function() {
-      done(new Error('unexpected success'));
-    }).catch(function(err) {
-      expect(err.message).to.include('Received bad response');
-      done();
-    }).catch(done);
-  });
+    it('handles already validated add-ons', function(done) {
+      var self = this;
+      var waitForSignedAddon = new CallableMock();
+      this.client.waitForSignedAddon = waitForSignedAddon.getCallable();
 
-  it('rejects the request promise on < 200 responses', function(done) {
-    this.client._request = new MockRequest({httpResponse: {statusCode: 122}});
-    this.client.get({url: '/something'}).then(function() {
-      done(new Error('unexpected success'));
-    }).catch(function(err) {
-      expect(err.message).to.include('Received bad response');
-      done();
-    }).catch(done);
-  });
+      this.client._request = new MockRequest({
+        httpResponse: {statusCode: 409},
+        responseBody: {error: 'version already exists'},
+      });
 
-  it('rejects the request promise with callback error', function(done) {
-    var callbackError = new Error('some error');
-    this.client._request = new MockRequest({responseError: callbackError});
+      this.sign().then(function(result) {
+        expect(waitForSignedAddon.wasCalled).to.be.equal(false);
+        expect(result.success).to.be.equal(false);
+        done();
+      }).catch(done);
+    });
 
-    this.client.get({url: '/something'}).then(function() {
-      done(new Error('unexpected success'));
-    }).catch(function(err) {
-      expect(err).to.be.equal(callbackError);
-      done();
-    }).catch(done);
-  });
+    it('throws an error when signing on a 500 server response', function(done) {
+      var self = this;
+      this.client._request = new MockRequest({httpResponse: {statusCode: 500}});
 
-  it('resolves the request promise with the HTTP response', function(done) {
-    var httpResponse = {statusCode: 201};
-    this.client._request = new MockRequest({httpResponse: httpResponse});
+      this.sign().then(function() {
+        done(new Error('unexpected success'));
+      }).catch(function(err) {
+        expect(err.message).to.include('Received bad response');
+        done();
+      }).catch(done);
+    });
 
-    this.client.get({url: '/something'}).then(function(responseResult) {
-      var returnedResponse = responseResult[0];
-      expect(returnedResponse).to.be.equal(httpResponse);
-      done();
-    }).catch(done);
-  });
+    it('waits for passing validation', function(done) {
+      var self = this;
+      this.client._request = new MockRequest({
+        responseQueue: [
+          signedResponse({valid: false, processed: false}),
+          signedResponse({valid: true, processed: true}),
+        ],
+      });
 
-  it('resolves the request promise with the response body', function(done) {
-    var responseBody = 'some text response';
-    this.client._request = new MockRequest({responseBody: responseBody});
+      var statusUrl = '/addons/something/versions/1.2.3/';
+      this.client.waitForSignedAddon(statusUrl, {
+        setAbortTimeout: function() {},
+      }).then(function(result) {
+        // Expect exactly two GETs before resolution.
+        expect(self.client._request.calls.length).to.be.equal(2);
+        expect(self.client._request.calls[0].conf.url).to.include(
+          statusUrl);
+        expect(result.success).to.be.equal(true);
+        done();
+      }).catch(done);
+    });
 
-    this.client.get({url: '/something'}).then(function(responseResult) {
-      var returnedBody = responseResult[1];
-      expect(returnedBody).to.be.equal(responseBody);
-      done();
-    }).catch(done);
-  });
+    it('waits for failing validation', function(done) {
+      var self = this;
+      this.client._request = new MockRequest({
+        responseQueue: [
+          signedResponse({valid: false, processed: false}),
+          signedResponse({valid: false, processed: true}),
+        ],
+      });
 
-  it('resolves the request promise with a JSON object', function(done) {
-    var data = {someKey: 'some value'};
+      this.client.waitForSignedAddon('/status-url/', {
+        setAbortTimeout: function() {},
+      }).then(function(result) {
+        // Expect exactly two GETs before resolution.
+        expect(self.client._request.calls.length).to.be.equal(2);
+        expect(result.success).to.be.equal(false);
+        done();
+      }).catch(done);
+    });
 
-    this.client._request = new MockRequest({
-      responseBody: JSON.stringify(data),
-      httpResponse: {
-        statusCode: 200,
-        headers: {
-          "content-type": "application/json",
+    it('aborts validation check after timeout', function(done) {
+      var clearTimeout = new CallableMock();
+
+      this.client.waitForSignedAddon('/status-url', {
+        clearTimeout: clearTimeout.getCallable(),
+        setStatusCheckTimeout: function() {
+          return 'status-check-timeout-id';
         },
+        abortAfter: 0,
+      }).then(function() {
+        done(new Error('Unexpected success'));
+      }).catch(function(err) {
+        expect(err.message).to.include('took too long');
+        expect(clearTimeout.call[0]).to.be.equal('status-check-timeout-id');
+        done();
+      }).catch(done);
+    });
+
+    it('clears abort timeout after resolution', function(done) {
+      var clearTimeout = new CallableMock();
+      var self = this;
+      this.client._request = new MockRequest({
+        responseQueue: [
+          signedResponse(),
+        ],
+      });
+
+      this.client.waitForSignedAddon('/status-url/', {
+        clearTimeout: clearTimeout.getCallable(),
+        setAbortTimeout: function() {
+          return 'abort-timeout-id';
+        },
+        setStatusCheckTimeout: function() {
+          return 'status-check-timeout-id';
+        },
+      }).then(function() {
+        expect(clearTimeout.call[0]).to.be.equal('abort-timeout-id');
+        done();
+      }).catch(done);
+    });
+  });
+
+
+  describe('requests', function() {
+
+    beforeEach(function() {
+      setUp.call(this);
+    });
+
+    it('makes requests with an auth token', function(done) {
+      var self = this;
+
+      this.client.get({url: '/somewhere'}).then(function() {
+        var call = self.client._request.calls[0];
+        var headerMatch = call.conf.headers.Authorization.match(/JWT (.*)/);
+        var token = headerMatch[1];
+        var data = jwt.verify(token, self.client.apiSecret);
+        expect(data.iss).to.be.equal(self.client.apiKey);
+        expect(data).to.have.keys(['iss', 'iat', 'exp']);
+        done();
+      }).catch(done);
+    });
+
+    it('can make any HTTP request', function(done) {
+      var self = this;
+      var requests = [];
+      ['get', 'put', 'post', 'patch', 'delete'].forEach(function(method) {
+        var urlPath = '/some/path';
+
+        requests.push(self.client[method]({url: urlPath}).then(function() {
+          var call = self.client._request.callMap[method];
+          expect(call.conf.url).to.be.equal(self.client.apiUrlPrefix + urlPath);
+          expect(call.conf.headers).to.have.keys(['Accept', 'Authorization']);
+        }));
+
+      });
+      when.all(requests).then(function() { done() }).catch(done);
+    });
+
+    it('requires a URL', function(done) {
+      this.client.get({}).then(function() {
+        done(new Error('unexpected success'));
+      }).catch(function(err) {
+        expect(err.message).to.include('URL was not specified');
+        done();
+      }).catch(done);
+    });
+
+    it('rejects the request promise on > 200 responses', function(done) {
+      this.client._request = new MockRequest({httpResponse: {statusCode: 409}});
+      this.client.get({url: '/something'}).then(function() {
+        done(new Error('unexpected success'));
+      }).catch(function(err) {
+        expect(err.message).to.include('Received bad response');
+        done();
+      }).catch(done);
+    });
+
+    it('rejects the request promise on < 200 responses', function(done) {
+      this.client._request = new MockRequest({httpResponse: {statusCode: 122}});
+      this.client.get({url: '/something'}).then(function() {
+        done(new Error('unexpected success'));
+      }).catch(function(err) {
+        expect(err.message).to.include('Received bad response');
+        done();
+      }).catch(done);
+    });
+
+    it('rejects the request promise with callback error', function(done) {
+      var callbackError = new Error('some error');
+      this.client._request = new MockRequest({responseError: callbackError});
+
+      this.client.get({url: '/something'}).then(function() {
+        done(new Error('unexpected success'));
+      }).catch(function(err) {
+        expect(err).to.be.equal(callbackError);
+        done();
+      }).catch(done);
+    });
+
+    it('can be configured not to throw on a bad response status', function(done) {
+      this.client._request = new MockRequest({httpResponse: {statusCode: 409}});
+      this.client.get({
+        url: '/something',
+      }, {
+        throwOnBadResponse: false,
+      }).then(function(result) {
+        expect(result[0].statusCode).to.be.equal(409);
+        done();
+      }).catch(done);
+    });
+
+    it('resolves the request promise with the HTTP response', function(done) {
+      var httpResponse = {statusCode: 201};
+      this.client._request = new MockRequest({httpResponse: httpResponse});
+
+      this.client.get({url: '/something'}).then(function(responseResult) {
+        var returnedResponse = responseResult[0];
+        expect(returnedResponse).to.be.equal(httpResponse);
+        done();
+      }).catch(done);
+    });
+
+    it('resolves the request promise with the response body', function(done) {
+      var responseBody = 'some text response';
+      this.client._request = new MockRequest({responseBody: responseBody});
+
+      this.client.get({url: '/something'}).then(function(responseResult) {
+        var returnedBody = responseResult[1];
+        expect(returnedBody).to.be.equal(responseBody);
+        done();
+      }).catch(done);
+    });
+
+    it('resolves the request promise with a JSON object', function(done) {
+      var data = {someKey: 'some value'};
+
+      this.client._request = new MockRequest({
+        responseBody: JSON.stringify(data),
+        httpResponse: {
+          statusCode: 200,
+          headers: {
+            "content-type": "application/json",
+          },
+        },
+      });
+
+      this.client.get({url: '/something'}).then(function(responseResult) {
+        var result = responseResult[1];
+        expect(result).to.deep.equal(data);
+        done();
+      }).catch(done);
+    });
+
+    it('ignores broken JSON responses', function(done) {
+      this.client._request = new MockRequest({
+        responseBody: '}{',  // broken JSON
+        httpResponse: {
+          statusCode: 200,
+          headers: {
+            "content-type": "application/json",
+          },
+        },
+      });
+
+      this.client.get({url: '/something'}).then(function(responseResult) {
+        var result = responseResult[1];
+        expect(result).to.be.a('string');
+        done();
+      }).catch(done);
+    });
+  });
+});
+
+
+describe('amoClient.PseudoProgress', function() {
+
+  beforeEach(function() {
+    this.setIntervalMock = new CallableMock({
+      returnValue: 'interval-id',
+    });
+    this.clearIntervalMock = new CallableMock();
+
+    this.progress = new amoClient.PseudoProgress({
+      setInterval: this.setIntervalMock.getCallable(),
+      clearInterval: this.clearIntervalMock.getCallable(),
+      stdout: {
+        columns: 80,
+        isTTY: true,
+        write: function() {},
       },
     });
-
-    this.client.get({url: '/something'}).then(function(responseResult) {
-      var result = responseResult[1];
-      expect(result).to.deep.equal(data);
-      done();
-    }).catch(done);
   });
 
-  it('ignores broken JSON responses', function(done) {
-    this.client._request = new MockRequest({
-      responseBody: '}{',  // broken JSON
-      httpResponse: {
-        statusCode: 200,
-        headers: {
-          "content-type": "application/json",
-        },
-      },
-    });
-
-    this.client.get({url: '/something'}).then(function(responseResult) {
-      var result = responseResult[1];
-      expect(result).to.be.a('string');
-      done();
-    }).catch(done);
+  it('should set an interval', function() {
+    this.progress.animate();
+    expect(this.setIntervalMock.wasCalled).to.be.equal(true);
   });
 
+  it('should clear an interval', function() {
+    this.progress.animate();
+    expect(this.setIntervalMock.wasCalled).to.be.equal(true);
+    this.progress.finish();
+    expect(this.clearIntervalMock.call[0]).to.be.equal('interval-id');
+  });
 });
 
 
 function MockRequest(conf) {
-  conf = _.assign({}, {
+  var self = this;
+  var defaultResponse = {
     httpResponse: {statusCode: 200},
     responseBody: '',
     responseError: null,
+  };
+  conf = _.assign({
+    // By default, responses will not be queued.
+    // I.E. the same response will be returned repeatedly.
+    responseQueue: false,
   }, conf);
+
+  this.responseQueue = conf.responseQueue;
+  this.returnMultipleResponses = !!this.responseQueue;
+
+  if (!this.returnMultipleResponses) {
+    // If the caller did not queue some responses then assume all
+    // configuration should apply to the response.
+    this.responseQueue = [conf];
+  }
+
+  // Make sure each queued response has the default values.
+  this.responseQueue.forEach(function(response, i) {
+    self.responseQueue[i] = _.assign({}, defaultResponse, response);
+  });
 
   this.calls = [];
   this.callMap = {};
@@ -203,7 +424,20 @@ MockRequest.prototype._mockRequest = function(method, conf, callback) {
   var info = {conf: conf};
   this.calls.push(_.assign({}, info, {name: method}));
   this.callMap[method] = info;
-  callback(this.responseError, this.httpResponse, this.responseBody);
+  //console.log('MockRequest:', method, conf.url);
+
+  var response;
+  if (this.returnMultipleResponses) {
+    response = this.responseQueue.shift();
+  } else {
+    // Always return the same response.
+    response = this.responseQueue[0];
+  }
+  if (!response) {
+    response = {};
+    response.responseError = new Error('Response queue is empty');
+  }
+  callback(response.responseError, response.httpResponse, response.responseBody);
 };
 
 MockRequest.prototype.get = function(conf, callback) {
@@ -224,4 +458,30 @@ MockRequest.prototype.patch = function(conf, callback) {
 
 MockRequest.prototype['delete'] = function(conf, callback) {
   return this._mockRequest('delete', conf, callback);
+};
+
+
+function MockProgress() {};
+
+MockProgress.prototype.animate = function() {};
+MockProgress.prototype.finish = function() {};
+
+
+function CallableMock(conf) {
+  conf = _.assign({
+    returnValue: undefined,
+  }, conf);
+  this.call = null;
+  this.wasCalled = false;
+  this.returnValue = conf.returnValue;
+}
+
+CallableMock.prototype._call = function() {
+  this.call = arguments;
+  this.wasCalled = true;
+  return this.returnValue;
+};
+
+CallableMock.prototype.getCallable = function() {
+  return this._call.bind(this);
 };
