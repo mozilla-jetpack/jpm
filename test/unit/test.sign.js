mozilla-jetpack/jpm

commit af0859c5bc7585dbbec65a45866bb90d1cf5b824

Fix passing of `addonDir` option
The jpm sign command takes separate `program` and `options` objects; the latter has options for the sub-command.

lib/sign.js
@@ -42,7 +42,7 @@ function sign (program, options, config) {
     }
     cmd.validateProgram(program);
 
-    var withManifest = config.getManifest({addonDir: options.addonDir, xpiPath: options.xpi})
+    var withManifest = config.getManifest({addonDir: program.addonDir, xpiPath: options.xpi})
       .then(function(manifest) {
         var xpiConfig = {
           manifest: manifest,

commit 71effc0b9981d3f2f776069e0b1211113145ae94

Fix up jpm sign after addonDir breakage
Fixes some fallout from commit af0859c5bc7585dbbec65a45866bb90d1cf5b824

lib/sign.js
@@ -16,6 +16,7 @@ var xpi = require("./xpi");
 
 function sign (program, options, config) {
   config = _.assign({
+    createXPI: xpi,
     getManifest: utils.getManifest,
     systemProcess: process,
     AMOClient: DefaultAMOClient,
@@ -51,7 +52,9 @@ function sign (program, options, config) {
           logger.log("Signing XPI: " + options.xpi);
           return _.assign(xpiConfig, {xpiPath: options.xpi});
         } else {
-          return xpi(manifest, options).then(function(xpiPath) {
+          // Merge the program and sub-command options.
+          var xpiOptions = _.assign({}, program, options);
+          return config.createXPI(manifest, xpiOptions).then(function(xpiPath) {
             logger.log("Created XPI for signing: " + xpiPath);
             return _.assign(xpiConfig, {xpiPath: xpiPath});
           });

test/unit/test.sign.js
@@ -653,15 +653,17 @@ describe('sign', function() {
   function runSignCmd(options) {
     options = _.assign({
       getManifest: null,
+      createXPI: null,
       StubAMOClient: makeAMOClientStub(),
       cmdOptions: {
         apiKey: 'some-key',
         apiSecret: 'some-secret',
-        addonDir: simpleAddonPath
+      },
+      program: {
+        addonDir: simpleAddonPath,
       },
     }, options);
 
-    var program = {};
     var cmdConfig = {
       systemProcess: mockProcess,
       AMOClient: options.StubAMOClient,
@@ -669,8 +671,11 @@ describe('sign', function() {
     if (options.getManifest !== null) {
       cmdConfig.getManifest = options.getManifest;
     }
+    if (options.createXPI !== null) {
+      cmdConfig.createXPI = options.createXPI;
+    }
 
-    return signCmd(program, options.cmdOptions, cmdConfig);
+    return signCmd(options.program, options.cmdOptions, cmdConfig);
   }
 
   it('should exit 0 on signing success', function(done) {
@@ -701,6 +706,22 @@ describe('sign', function() {
       },
     }).then(function() {
       expect(mockProcessExit.call[0]).to.be.equal(1);
+      done();
+    }).catch(done);
+  });
+
+  it('passes addonDir to XPI creator', function(done) {
+    var mockXPICreator = new CallableMock();
+    runSignCmd({
+      createXPI: mockXPICreator.getCallable(),
+      program: {
+        addonDir: "/nowhere/stub/xpi/path",
+      }
+    }).then(function() {
+      expect(mockXPICreator.call[1].addonDir).to.be.equal("/nowhere/stub/xpi/path");
+      // Note that the stub XPI creator doesn't return a promise so it raises
+      // an exception which we are ignoring for this test (famous last words).
+      // The promise still resolves because that's how the command works.
       done();
     }).catch(done);
   });