e110c7f8c11b6fd1e3f968e698071c4fe5f1a265
Kumar McMillan <kumar.mcmillan@gmail.com>
2016-09-30 12:32:48 -0500

    test: fixed async and cleanup bugs with some XPI unit tests

--- test/unit/test.xpi.js
+++ test/unit/test.xpi.js
@@ -20,7 +20,6 @@ var jpmignoreLFPath = path.join(__dirname, "..", "addons", "jpmignore-lf");
 var jpmignoreCRLFPath = path.join(__dirname, "..", "addons", "jpmignore-crlf");
 var jpmignoreMixedPath = path.join(__dirname, "..", "addons", "jpmignore-mixed");
 var tmpOutputDir = path.join(__dirname, "../", "tmp");
-var targetDir = path.join(__dirname, "../", "target");
 var updateRDFPath = path.join(__dirname, "..", "fixtures", "updateRDF");
 var updateRDFFailPath = path.join(__dirname, "..", "fixtures", "updateRDF-fail");
 var webextensionManifestExcludedPath = path.join(
@@ -37,38 +36,44 @@ describe("lib/xpi", function() {
   beforeEach(utils.setup);
   afterEach(utils.tearDown);
 
-  it("Zips up cwd's addon", function(done) {
+  it("Zips up cwd's addon", function() {
     process.chdir(simpleAddonPath);
     var manifest = require(path.join(simpleAddonPath, "package.json"));
     var xpiPath;
-    return xpi(manifest).then(function(filePath) {
-      xpiPath = filePath;
-      expect(xpiPath).to.be.equal(path.join(simpleAddonPath,
-                                            "simple-addon.xpi"));
-      return utils.unzipTo(xpiPath, tmpOutputDir).then(function() {
-        utils.compareDirs(simpleAddonPath, tmpOutputDir);
+    return xpi(manifest)
+      .then(function(filePath) {
+        xpiPath = filePath;
+        expect(xpiPath).to.be.equal(path.join(simpleAddonPath,
+                                              "simple-addon.xpi"));
+        return utils.unzipTo(xpiPath, tmpOutputDir);
+      })
+      .then(function() {
+        return utils.compareDirs(simpleAddonPath, tmpOutputDir);
+      })
+      .then(function() {
+        return fs.unlink(xpiPath);
       });
-    })
-    .then(function() {
-      fs.unlink(xpiPath);
-      done();
-    })
-    .catch(done);
   });
 
   it("Zips up cwd's addon in the specified directory", function() {
     process.chdir(simpleAddonPath);
     var manifest = require(path.join(simpleAddonPath, "package.json"));
-    fs.mkdirs(targetDir);
-    return xpi(manifest, {destDir: targetDir}).then(function(filePath) {
-      expect(filePath).to.be.equal(path.join(targetDir, "simple-addon.xpi"));
-      return utils.unzipTo(filePath, tmpOutputDir).then(function() {
+    var destDir = path.join(tmpOutputDir, "jpm-tmp-dest-dir");
+
+    return fs.mkdirs(destDir)
+      .then(function() {
+        return xpi(manifest, {destDir: destDir});
+      })
+      .then(function(filePath) {
+        expect(filePath).to.be.equal(path.join(destDir, "simple-addon.xpi"));
+        return utils.unzipTo(filePath, tmpOutputDir);
+      })
+      .then(function() {
+        return fs.remove(destDir);
+      })
+      .then(function() {
         utils.compareDirs(simpleAddonPath, tmpOutputDir);
       });
-    })
-    .then(function() {
-      fs.remove(targetDir);
-    });
   });
 
   it("Check file name with id", function(done) {
