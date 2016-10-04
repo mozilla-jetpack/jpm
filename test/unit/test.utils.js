a538c9d5c3734eb93aa587d98249a3eb501ae0d7
Martin Giger <martin@humanoids.be>
2016-10-04 21:26:51 +0200

    fix: Fixed bootstrap.js & install.rdf not generated for Firefox >=50.0a (#592)

--- test/unit/test.utils.js
+++ test/unit/test.utils.js
@@ -8,7 +8,6 @@ var path = require("path");
 var chai = require("chai");
 var expect = chai.expect;
 var utils = require("../../lib/utils");
-var hasAOMSupport = utils.hasAOMSupport;
 
 var simpleAddonPath = path.join(__dirname, "..", "addons", "simple-addon");
 var simpleAddonXPI = path.join(__dirname, "..", "xpis", "@simple-addon.xpi");
@@ -270,35 +269,5 @@ describe("lib/utils", function() {
         });
     });
 
-  });
-
-  describe("hasAOMSupport", function() {
-    it("hasAOMSupport true for valid ranges", function() {
-      [">=51 <=54", ">=50.0a <=52", ">=50", ">=51.0a"].forEach(function(range) {
-        expect(hasAOMSupport({ engines: { "firefox": range } })).to.be.equal(true);
-      });
-    });
-    it("hasAOMSupport false for invalid ranges", function() {
-      [">=28 <=34", ">=30 <=32", ">=26", ">=30.0a", ">=38 <=44"].forEach(function(range) {
-        expect(hasAOMSupport({ engines: { "firefox": range } })).to.be.equal(false);
-      });
-    });
-    it("hasAOMSupport false for unspecified min", function() {
-      ["<26", "<=32", "<40.0a"].forEach(function(range) {
-        expect(hasAOMSupport({ engines: { "firefox": range } })).to.be.equal(false);
-      });
-    });
-    it("hasAOMSupport false for no engines field", function() {
-      expect(hasAOMSupport({})).to.be.equal(false);
-    });
-    it("hasAOMSupport false for unpopulated engines field", function() {
-      expect(hasAOMSupport({ engines: {}})).to.be.equal(false);
-    });
-    it("hasAOMSupport false for one valid and one invalid engine", function() {
-      expect(hasAOMSupport({ engines: {
-        "firefox": ">=40.0a",
-        "fennec": "<31"
-      }})).to.be.equal(false);
-    });
   });
 });
